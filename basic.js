9f616e54dd89ef00a541e0864497acdcdc28998a
jtalavera <jreeset@gmail.com>
2023-11-07 14:08:46 -0500

    add 3d model

--- basic.js
+++ basic.js
@@ -7,13 +7,14 @@ window.onload = () => {
         if(!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
             // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-box");
+            const entity = document.createElement("a-entity");
             entity.setAttribute("scale", {
                 x: 20, 
                 y: 20,
                 z: 20
             });
-            entity.setAttribute('material', { color: 'red' } );
+            entity.setAttribute("gltf-model", "map_pointer/scene.gltf")
+            //entity.setAttribute('material', { color: 'red' } );
             entity.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
