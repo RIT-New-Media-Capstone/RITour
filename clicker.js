7522e1a80dae25ed49d9e77176ae21e2bebde468
Max Lama <mkl1328@rit.edu>
2023-11-09 12:37:32 -0500

    debug for clicker

--- clicker.js
+++ clicker.js
@@ -3,6 +3,7 @@ AFRAME.registerComponent('clicker', {
       const cameraEl = document.querySelector('a-camera');
       let position, cameraPos;
       this.el.addEventListener('click', e => {
+        console.dir(this.el);
           position = this.el.object3D.position; 
           cameraPos = cameraEl.object3D.position;
           alert(`Box clicked! Position is: ${position.x} ${position.y} ${position.z}; camera pos ${cameraPos.x} ${cameraPos.y} ${cameraPos.z}`);
