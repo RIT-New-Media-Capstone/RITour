d65af72a52459915946ac94e909acb7fee5c5058
Max Lama <mkl1328@rit.edu>
2023-11-08 19:06:31 -0500

    No event listenr for camera move

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ window.onload = () => {
   let testEntitiesAdded = false;
   // alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
   const el = document.querySelector("[gps-new-camera]");
-  el.addEventListener("gps-camera-update-position", e => {
+  // el.addEventListener("gps-camera-update-position", e => {
       if(!testEntitiesAdded) {
-          alert(`Initial position: ${e.detail.position.longitude} ${e.detail.position.latitude}`);
+          // alert(`Initial position: ${e.detail.position.longitude} ${e.detail.position.latitude}`);
       
         const data = {
           "Orange Hall": {
@@ -42,17 +42,33 @@ window.onload = () => {
               "type": "Dining",
               "lat": 43.084,
               "long": -77.680
-          },
-          "Thomas Gosnell Hall": {
+            },
+            "Thomas Gosnell Hall": {
               "name": "Thomas Gosnell Hall",
               "desription": "An academic building that houses classrooms, labs and department offices for the College of Science, with undergraduate and graduate programs in mathematics, biology, bioinformatics, physics and medical sciences.",
               "type": "Academic Building",
               "lat": 43.083,
               "long": -77.677
+            }
           }
-        }
 
-        // Change these names
+          // console.log("about to make entity")
+          // const entity = document.createElement("a-text");
+          //     entity.setAttribute("value", `Test Title`)
+          //     entity.setAttribute("scale", {
+          //         x: 120, 
+          //         y: 120,
+          //         z: 120
+          //     });
+          //     entity.setAttribute('gps-new-entity-place', {
+          //         latitude: 43.083,
+          //         longitude: -77.676,
+          //     });
+          //     entity.setAttribute('clicker', { });                
+          //     document.querySelector("a-scene").appendChild(entity);
+          
+          //     console.log("entity made")
+          // Change these names
           for(const prop in data) {
               const entity = document.createElement("a-text");
               entity.setAttribute("value", `${data[prop].name}`)
@@ -68,9 +84,10 @@ window.onload = () => {
               entity.setAttribute('clicker', { });                
               document.querySelector("a-scene").appendChild(entity);
           }
+
           testEntitiesAdded = true;
       }
-  });
+  // });
 
   // document.getElementById("go").addEventListener("click", e=> {
   //     const lat = document.getElementById('lat').value;
